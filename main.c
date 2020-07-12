8657b14cfe9abd8d7d7e39b84f9266ffa8d4f6aa
krisjanisbergmanis95 <58779450+krisjanisbergmanis95@users.noreply.github.com>
2020-07-12 17:54:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,32 +4,122 @@
 #include <math.h>
 #include <stdio.h>
 #include <avr/interrupt.h>
-
+#include <avr/delay.h>
 
 
 void PortInit();
+void Init_ExternalInterupts();
 void USARTInit(int mode);
 void USART_Transmit(char val);
 void USART_Transmit_String(char *);
-int main(void)
-{
-	PortInit();
+uint8_t USART_Read_Byte;
+enum INPUT {coin, push, waiting};
+enum INPUT input_action = waiting;
+	
+enum STATE {locked, unlocked};
+enum STATE next_state = locked;
 
-	USARTInit(1);
+void checkState();
+void checkInput();
+
+int write_mode = 0;
+int main(void){
+
+	
+	PortInit();
+USARTInit(0);
+	//USARTInit(1);
 	sei ();
 	
-	while (1)
-	{
-		//USART_Transmit('a');
-	USART_Transmit_String("test hello this is a message");
+	while (1) {
+		USARTInit(0);
+		checkInput();
+// 		if (write_mode == 1) {
+// 			USARTInit(1);
+// 			checkState();
+// 			write_mode = 0;
+// 		}
+		USARTInit(1);
+		checkState();
+		
+		//USART_Transmit_String("12");
 	}
+
 }
+
+void checkState() {
+	write_mode = 0;
+	switch(next_state) {
+		
+		
+		case locked:
+		if (input_action == coin) {
+			next_state = unlocked;
+			input_action = waiting;
+			USARTInit(1);
+			USART_Transmit_String("A");
+// 			USART_Transmit_String("current state: locked");
+// 			USART_Transmit_String("next state: unlocked");
+// 			USART_Transmit_String("unlocked - you can go");
+		}
+		else if (input_action == push) {
+			next_state = locked;
+			input_action = waiting;
+			USARTInit(1);
+			USART_Transmit_String("B");
+// 			USART_Transmit_String("current state: locked");
+// 			USART_Transmit_String("next state: locked");
+// 			USART_Transmit_String("locked - insert coin");
+		}
+		break;
+		case unlocked:
+		if (input_action == coin) {
+			next_state = unlocked;
+			input_action = waiting;
+			USARTInit(1);
+			USART_Transmit_String("C");
+// 			USART_Transmit_String("current state: unlocked");
+// 			USART_Transmit_String("next state: unlocked");
+// 			USART_Transmit_String("unlocked already - you can go");
+		}
+		else if (input_action == push) {
+			next_state = locked;
+			input_action = waiting;
+			USARTInit(1);
+			USART_Transmit_String("D");
+// 			USART_Transmit_String("current state: unlocked");
+// 			USART_Transmit_String("next state: locked");
+// 			USART_Transmit_String("enjoy. locking after");
+		}
+		break;
+		USARTInit(0);
+	}
+
+}
+
+void checkInput() {
+	if (USART_Read_Byte == 'c') {
+		input_action = coin;
+		
+	} else if (USART_Read_Byte == 'p') {
+		input_action = push;
+		
+	}
+	else {
+		input_action = waiting;
+		//write_mode = 0;
+	}
+	
+}
+
 
 /*-------------INIT----------------*/
 void PortInit()
 {
-	DDRD = 0xFF;
-	DDRB = 0x2F;
+	DDRD = 0b00000000;
+	PORTD = 0b00001100;
+	DDRB = 0b00100000;
+	PORTB = 0b00000000;
 }
 
 void USARTInit(int mode) {
@@ -47,10 +137,30 @@ void USARTInit(int mode) {
 	}	
 }
 
+Init_ExternalInterupts(){
+	EICRA |= (1 << ISC11) | (1 << ISC10) | (1 << ISC01) | (1 << ISC00);
+	EIMSK |= (1<< INT1) | (1 <<INT0);
+}
+
 /*^^------------INIT----------------*/
 
 /*-------------ISR----------------*/
+ISR(INT0_vect) {
+	input_action = coin;
+}
 
+ISR(INT1_vect) {
+	input_action = push;
+}
+
+ISR(USART_RX_vect){
+	write_mode = 1;
+	
+	USART_Read_Byte = UDR0;
+	PORTB ^= (1 << PORTB5);
+	
+	
+}
 /*^^-----------ISR----------------*/
 
 
@@ -62,7 +172,7 @@ void USART_Transmit(char val) {
 }
 
 void USART_Transmit_String(char *val) {
-	while(*val != '\0') {
+	while(*val != NULL) {//'\0'
 		USART_Transmit(*val);
 		val++;
 	}
