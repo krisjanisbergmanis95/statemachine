29af503b2a6c6c562e3e097b12b958535bf64f5b
krisjanisbergmanis95 <58779450+krisjanisbergmanis95@users.noreply.github.com>
2020-11-03 23:59:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,54 +3,340 @@
 #include <avr/io.h>
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 #include <avr/interrupt.h>
-
+#include <avr/delay.h>
 
 
 void PortInit();
-void USARTInit(int mode);
+void Init_ExternalInterupts();
+void USARTInit();
 void USART_Transmit(char val);
 void USART_Transmit_String(char *);
-int main(void)
-{
+uint8_t USART_Read_Byte;
+char USARTReadBufferArr[16] = {'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'};
+	
+
+
+char *LEVEL_ONE_PIN = "123";
+//char LEVEL_ONE_PIN[3] = {'1','2','3'};
+// char *ACTION_MOVE = "move";
+// char *ACTION_PIN = "pin1";
+
+char *ACTION_MOVE = "mov";
+char *ACTION_PIN = "pin";
+
+char *MOVE_RIGHT = "-->";
+char *MOVE_LEFT = "<--";
+
+
+int usart_read_index = 0;
+int usart_read_index_action = 0;
+int usart_read_index_move = 0;
+char pin_to_check_level_one[3] = {};
+char move_to_check[3] = {};
+char action_to_check[4] = {};
+
+int global_pincheck = 55;
+int global_move_check = 29;
+
+int incorrect_pin_counters = 0;
+enum INPUT {pin_correct, pin_incorrect, waiting};
+enum INPUT input_action = waiting;
+
+enum ACTION {pin, move, wait};
+enum ACTION to_do = wait;
+
+enum MOVE {forward, back, stay};
+enum MOVE go_there = stay;
+
+enum STATE {state1, state2, state3, state4, state5,state6};
+enum STATE next_state = state1;
+
+void checkAction();
+void checkInput();
+void checkState();
+
+void echo();
+
+void clear_pin_to_check_level_one();
+void clear_action_to_check();
+void clear_move_to_check();
+
+int main(void){
+	USARTInit();
+	
 	PortInit();
 
-	USARTInit(1);
 	sei ();
-	
-	while (1)
-	{
-		//USART_Transmit('a');
-	USART_Transmit_String("test hello this is a message");
+	clear_pin_to_check_level_one();
+	clear_move_to_check();
+	while (1) {
+		checkAction();
+		checkInput();
+		checkState();
 	}
+
+}
+
+void checkState() {
+	switch(next_state) {
+		
+		
+		case state1:
+		USART_Transmit_String("current state 1");
+		if (incorrect_pin_counters < 3) {
+			next_state = state2;
+			input_action = waiting;
+			USART_Transmit_String("next state 2");
+		}
+		else {
+			next_state = state1;
+			input_action = waiting;
+			USART_Transmit_String("next state 4");
+		}
+		break;
+		
+		case state2:
+		USART_Transmit_String("current state 2");
+		if (input_action == pin_correct) {
+			next_state = state3;
+			input_action = waiting;
+			incorrect_pin_counters = 0;
+			USART_Transmit_String("next state 3");
+		
+		}
+		else if (input_action == pin_incorrect) {
+			next_state = state1;
+			input_action = waiting;
+			incorrect_pin_counters++;
+			USART_Transmit_String("next state 1");
+			
+		}
+		break;
+		
+		case state3:
+		USART_Transmit_String("current state 3");
+		if (go_there == forward) {
+			next_state = state6;
+			input_action = waiting;
+			USART_Transmit_String("next state 6");
+			
+		}
+		else if (go_there == back) {
+		next_state = state1;
+		input_action = waiting;
+		USART_Transmit_String("next state 1");
+		}
+		
+		break;
+		
+		case state4:
+		USART_Transmit_String("current state 4");
+		if (input_action == pin_correct) {
+			next_state = state1;
+			input_action = waiting;
+			incorrect_pin_counters = 0;
+			USART_Transmit_String("next state 1");
+			
+		}
+		else if (input_action == pin_incorrect) {
+			next_state = state4;
+			input_action = waiting;
+			USART_Transmit_String("next state 5");
+			
+		}
+		break;
+		
+		
+		case state5:
+		USART_Transmit_String("current state 5");
+		
+			next_state = state5;
+			input_action = waiting;
+			USART_Transmit_String("ALARM! LED0");
+			
+		break;
+		
+		case state6:
+		USART_Transmit_String("current state 6");
+		if (go_there == forward) {
+			next_state = state6;
+			input_action = waiting;
+			USART_Transmit_String("next state 7; TODO");
+			
+		}
+		else if (go_there == back) {
+			next_state = state3;
+			input_action = waiting;
+			USART_Transmit_String("next state 3");
+		}
+		
+		break;
+		
+		
+	}
+
+}
+
+
+void checkAction() {
+	
+	int testr = 11000;
+	testr = strcmp(action_to_check, ACTION_PIN);
+	
+	if (testr == 0 && to_do != pin) {
+		USART_Transmit_String("WAITING FOR PIN\n");
+		clear_pin_to_check_level_one();
+		usart_read_index = 0;
+		usart_read_index_move = 0;
+		usart_read_index_action = 0;
+		to_do = pin;
+	}
+	
+	testr = strcmp(action_to_check, ACTION_MOVE);
+	if (testr == 0 && to_do != move) {
+		USART_Transmit_String("WAITING FOR MOVE TODO\n");
+		usart_read_index = 0;
+		usart_read_index_move = 0;
+		usart_read_index_action = 0;
+		to_do = move;
+		clear_action_to_check();
+		clear_move_to_check();
+	}
+	clear_action_to_check();
+	//clear_move_to_check();
+	
+	usart_read_index = 0;
+	usart_read_index_move = 0;
+	usart_read_index_action = 0;
+}
+
+void checkInput() {
+	int result = 11000;
+	global_pincheck = strcmp(pin_to_check_level_one,LEVEL_ONE_PIN);
+	
+	switch(to_do) {
+		case pin:
+		
+			
+		USART_Transmit_String("PIN : ");
+		USART_Transmit_String(pin_to_check_level_one);
+		USART_Transmit_String("\n");
+
+			if (global_pincheck == 0) {
+				input_action = pin_correct;
+				clear_pin_to_check_level_one();
+				usart_read_index_action = 0;
+				
+				} else if (global_pincheck != 0 && pin_to_check_level_one[0] != NULL && pin_to_check_level_one[1] != NULL) {
+				input_action = pin_incorrect;
+				clear_pin_to_check_level_one();
+				usart_read_index_action = 0;
+			}
+			else {
+				input_action = waiting;
+			}
+			
+			break;
+		
+		case move: 
+			
+			global_move_check = strcmp(move_to_check, MOVE_RIGHT);
+			USART_Transmit_String("MOVE : ");
+			USART_Transmit_String(move_to_check);
+			USART_Transmit_String("\n");
+			if (global_move_check == 0 && go_there != forward) {
+				USART_Transmit_String("MOVING FORWARD\n");
+				clear_move_to_check();
+				clear_action_to_check();
+				usart_read_index = 0;
+				usart_read_index_move = 0;
+				usart_read_index_action = 0;
+				go_there = forward;
+			}
+			
+			global_move_check = strcmp(move_to_check, MOVE_LEFT);
+			if (global_move_check == 0 && go_there != back) {
+				USART_Transmit_String("GOING BACK\n");
+				usart_read_index = 0;
+				usart_read_index_action = 0;
+				usart_read_index_move = 0;
+				go_there = back;
+			}
+			usart_read_index_move = 0;
+			usart_read_index = 0;
+			usart_read_index_action = 0;
+			clear_move_to_check();
+			clear_action_to_check();
+		
+		break;
+		default:
+		clear_move_to_check();
+		clear_action_to_check();
+		clear_pin_to_check_level_one();
+		usart_read_index_action = 0;
+		usart_read_index = 0;
+		usart_read_index_move = 0;
+	}
+
+	
+	
+	_delay_ms(500);
 }
 
 /*-------------INIT----------------*/
 void PortInit()
 {
-	DDRD = 0xFF;
-	DDRB = 0x2F;
+	DDRD = 0b00000000;
+	PORTD = 0b00001100;
+	DDRB = 0b00100000;
+	PORTB = 0b00000000;
 }
 
-void USARTInit(int mode) {
-	// 	datu biti: 8
-	// STOP biti: 1
-	// paritāte: nav
-	// baud rate: 9600
+void USARTInit() {
 	UBRR0 = 103; //9600
-	if (mode == 0) { //receive moe
-		UCSR0B = (1 << RXEN0) | (1 << RXCIE0); //enable reciever and receive interupt
-		UCSR0C = (1 << UCSZ00) | (1 <<  UCSZ01); //8 data bits
-	} else if (mode == 1) {//transmit mode
-		UCSR0B = (1 << TXEN0) ; //enable transmitter
-		UCSR0C = (1 << UCSZ00) | (1 <<  UCSZ01); //8 data bits
-	}	
+	UCSR0B = (1 << RXEN0) | (1 << RXCIE0) | (1 << TXEN0) ; //enable reciever and receive interupt
+	UCSR0C = (1 << UCSZ00) | (1 <<  UCSZ01); //8 data bits
+}
+
+Init_ExternalInterupts(){
+	EICRA |= (1 << ISC11) | (1 << ISC10) | (1 << ISC01) | (1 << ISC00);
+	EIMSK |= (1<< INT1) | (1 <<INT0);
 }
 
 /*^^------------INIT----------------*/
 
 /*-------------ISR----------------*/
 
+ISR(USART_RX_vect){
+	char bufferChar = NULL;
+	bufferChar = UDR0;
+	
+	
+	action_to_check[usart_read_index_action] = bufferChar;
+	usart_read_index_action++;
+	if (usart_read_index_action > 2) {
+		usart_read_index_action = 0;
+	}
+	
+	if (to_do == move) {
+		move_to_check[usart_read_index_move] = bufferChar;
+		usart_read_index_move++;
+		if (usart_read_index_move > 2) {
+			usart_read_index_move = 0;
+			usart_read_index_action = 0;
+		}
+	}
+	if (to_do == pin) {
+		pin_to_check_level_one[usart_read_index] = bufferChar;
+		usart_read_index++;
+		if (usart_read_index > 2) {
+			usart_read_index = 0;
+		}
+		
+	}
+}
 /*^^-----------ISR----------------*/
 
 
@@ -62,8 +348,29 @@ void USART_Transmit(char val) {
 }
 
 void USART_Transmit_String(char *val) {
-	while(*val != '\0') {
+	while(*val != NULL) {
 		USART_Transmit(*val);
 		val++;
 	}
 }
+
+void echo() {
+	USART_Transmit_String(USARTReadBufferArr);
+	USART_Transmit_String("\n");
+}
+
+void clear_pin_to_check_level_one() {
+	pin_to_check_level_one[0] = NULL;
+	pin_to_check_level_one[1] = NULL;
+}
+
+void clear_action_to_check() {
+	action_to_check[0] = NULL;
+}
+
+
+void clear_move_to_check() {
+	
+	move_to_check[0] = NULL;
+}
+
